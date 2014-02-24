4a3eca02709ce908987a3504ca50df26e0854a60
Sean Dempsey <sean@loudcanvas.com>
2014-02-24 16:42:58 -0500

    updating example

--- examples/where_clause_test.php
+++ examples/where_clause_test.php
@@ -2,12 +2,7 @@
 
 	//for more documentation go to http://ajaxcrud.com/api/index.php?id=ajaxcrud_where_clause
 
-	require_once('../preheader.php'); // <-- this include file MUST go first before any HTML/output
-
-	#the code for the class
-	include ('../ajaxCRUD.class.php'); // <-- this include file MUST go first before any HTML/output
-
-	//print_r($_SESSION);
+	session_start(); //this is all i need to include for this demo
 
 	echo "<h2>Current WHERE Clauses in SESSION</h2><hr /<br /><br />\n";
 
@@ -16,15 +11,14 @@
 		echo "<p>Number of WHERE Clauses in session: $countWhereClauses</p>";
 		if ($countWhereClauses > 0){
 			foreach ($_SESSION['ajaxcrud_where_clause'] as $table => $whereClause){
-				$report_msg[] = "Table: <b>$table</b> | WHERE Clause: <i>$whereClause</i>";
+				echo "&nbsp;&nbsp;Table: <b>$table</b> | WHERE Clause: <i>$whereClause</i><br />";
 			}
 		}//if any where clauses exist
 	}//if any where clauses are
 	else{
-		$error_msg[] = "No WHERE clauses set by any ajaxCRUD table instances (yet); try going into an example and typing in a filter box; then come back and refresh this page.";
+		echo "<p>No WHERE clauses set by any ajaxCRUD table instances (yet); try going into an example and typing in a filter box; then come back and refresh this page.</p>\n";
 	}
-
-	echo_msg_box();
-
-
 ?>
+<br /><hr /><br />
+
+<p><i>What the hell is the point of this?</i><br /> This variable is really just used for advanced/special use cases; read doumentation here: <a href="http://ajaxcrud.com/api/index.php?id=ajaxcrud_where_clause">http://ajaxcrud.com/api/index.php?id=ajaxcrud_where_clause</a><p>
