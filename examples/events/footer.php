18132950a8f0292a5502d227ca918bc43a5dbe3a
Sean Dempsey <sean@loudcanvas.com>
2014-03-08 14:17:30 -0500

    updating example app which comes with package
    adding a more robust app example to package (Event Signup app)

--- examples/events/footer.php
+++ examples/events/footer.php
@@ -1,17 +1,17 @@
 	</div>
 </div>
 
-<div style="margin-left:auto; margin-right:auto; width:70%; background-color: #ffffff;">
+<div style="margin-left:auto; margin-right:auto; width:80%; background-color: #ffffff;">
 	<table border="0" align="center" cellpadding="0" cellspacing="0" style="width: 100%; background-color: #333333;">
 	  <tr align="center" valign="middle">
-		<td width="12%"><a href="http://<?=$globals['site_name']?>" class="menu"><?=$globals['site_name']?></a></td>
+		<td width="12%">Powered by <a href="http://ajaxcrud.com" class="menu">ajaxCRUD</a></td>
 		<td width="12%">&nbsp;</td>
 		<td width="12%">&nbsp;</td>
 		<td width="12%">&nbsp;</td>
 		<td width="12%">&nbsp;</td>
 		<td width="12%">&nbsp;</td>
 		<td width="12%">&nbsp;</td>
-		<td width="12%"><a href="index.php?action=logout" class="menu">Logout</a></td>
+		<td width="12%">&copy; <a href="http://www.loudcanvas.com" class="menu">LCM</a></td>
 	  </tr>
 	</table>
 </div>
